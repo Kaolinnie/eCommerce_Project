f0b51630d6b0588129205623266e73aae3a01e19
Kaolin <kaolin.stacey@gmail.com>
2022-11-23 18:27:42 -0500

    fixes

--- app/views/Layout/Header.php
+++ app/views/Layout/Header.php
@@ -1,29 +1,4 @@
 <header>
-    <a href='/' class="headerLogo"><img src="/resources/images/figma.svg" alt="" id="logo"></a>
-    <ul id="main_nav_links">
-        <!-- <li class="main_nav_link">
-            <a href="/">
-                <img src="/resources/images/home.png" alt="">
-            </a>
-        </li>
-        <li class="main_nav_link">
-            <a href="#">
-                <img src="/resources/images/search.png" alt="">
-            </a>
-        </li>
-        <li class="main_nav_link">
-            <a href="#">
-                <img src="/resources/images/about.png" alt="">
-            </a>
-        </li>
-        <li class="main_nav_link">
-            <a href="/Account">
-                <img src="/resources/images/profile.png" alt="">
-            </a>
-        </li> -->
-
-        
-    </ul>
     <div id="menunav">
         <div class="profile_div">
             <a href="" class="profile_pic"><img src="/images/page_header_image/pizza.jpg" alt=""></a>
@@ -35,7 +10,18 @@
             <span class="city">Chateauguay</span>
             <span class="province">Quebec</span>
         </div>
-
+        <div id="account">
+            <?php
+            if(isset($_SESSION['user_id'])){
+                echo "<button type='button' class='btn btn-warning' onclick=\"location.href='/Account/profile'\">Profile</button>";
+                echo "<button type='button' class='btn btn-info' onclick=\"location.href='/Account/logout'\">Logout</button>";
+            }
+            else {
+                echo  "<button type='button' class='btn btn-light' onclick=\"location.href='/Account/login'\">Sign In</button>";
+                echo  "<button type='button' class='btn btn-primary' onclick=\"location.href='/Account/register'\">Sign Up</button>";
+            }
+            ?>
+        </div>
 
         <a href="#" class="logout_link"><img src="/resources/images/logout.png" alt="" style="filter:invert(100%);-webkit-filter:invert(100%);"></a>
     </div>
@@ -48,16 +34,5 @@
         <a href='/' class="logo_link"><img src="/resources/images/logo.png" alt="logo"></a>
         <a href='#' class="checkout_link fixed_header"><img src="/resources/images/checkout.png" alt="checkout"></a>
     </div>
-    <div id="account">
-        <?php
-        if(isset($_SESSION['user_id'])){
-            echo "<button type='button' class='btn btn-warning' onclick=\"location.href='/Account/profile'\">Profile</button>";
-            echo "<button type='button' class='btn btn-info' onclick=\"location.href='/Account/logout'\">Logout</button>";
-        }
-        else {
-            echo  "<button type='button' class='btn btn-light' onclick=\"location.href='/Account/login'\">Sign In</button>";
-            echo  "<button type='button' class='btn btn-primary' onclick=\"location.href='/Account/register'\">Sign Up</button>";
-        }
-        ?>
-    </div>
+
 </header>
