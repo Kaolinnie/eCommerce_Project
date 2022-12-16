ddc64f81215e019125ee29b0905f77dbf604a34b
MercyWhistles <82103220+MercyWhistles@users.noreply.github.com>
2022-12-16 14:00:37 -0500

    register page css complete and php is working

--- app/views/Company/index.php
+++ app/views/Company/index.php
@@ -5,23 +5,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php $this->view('Layout/HeadLinks');?>
-    <link rel="stylesheet" href="/resources/styles/homeStyles.css">
+    <link rel="stylesheet" href="/resources/styles/companyPage.css">
     <title><?=_("Register Company")?></title>
 </head>
 <body>
     <?php $this->view('Layout/Header'); ?>
     <main>
+        <div class="content">
+            <div class="outerbox">
+                <div class="boxTitle">
         <h2><?=_("Register your Company")?></h2>
+        <small><?=_("Please note that this is only a request, you will receive an email confirmation when our team approves your company page!")?></small>
+                </div>
         <form action='' method="post">
-            <label><?=_("Owner's Name")?><input type="text" name="company_owner_name_input" required></label><br>
-            <label><?=_("Company Name")?><input type="text" name="company_name_input" required></label><br>
-            <label><?=_("Company Email")?><input type="text" name="company_email_input" required></label><br>
-            <label><?=_("Company Address")?><input type="text" name="company_address_input" required></label><br>
-            <label><?=_("Company Suite")?><input type="text" name="company_suite_input" placeholder="<?=_("(Optional)")?>"></label><br>
-            <label><?=_("Postal Code")?><input type="text" name="company_postal_code_input" required></label><br>
-            <input class="submitButton btn btn-primary" type="submit" value="Register" name="action" onsubmit="index()">
+
+            <div class="form-group">
+                <label for="ownerName">Company Owner</label>
+                <input type="text" class="form-control" name="company_owner_name_input" id="ownerName" require>
+            </div>
+
+            <div class="form-group">
+                <label for="companyName">Company Name</label>
+                <input type="text" class="form-control" name="company_name_input" id="companyName" require>
+            </div>
+
+            <div class="form-group">
+                <label for="companyEmail">Company Email</label>
+                <input type="email" class="form-control" name="company_email_input" id="companyEmail" require>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="companyAddress">Company Address</label>
+                <input type="text" class="form-control" name="company_address_input" id="companyAddress" require>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="companySuite">Company Suite</label>
+                    <input type="text" name="company_suite_input" class="form-control" id="companySuite" placeholder="Optional">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="companyPS">Postal Code</label>
+                    <input type="text" name="company_postal_code_input" class="form-control" id="companyPS" require>
+                </div>
+            </div>
+            <input class="submitButton btn btn-secondary btn-lg btn-block" type="submit" value="Register" name="action" onsubmit="index()">
         </form>
-        <p><?=_("Please note that this is only a request, you will receive an email confirmation when our team approves your company page!")?></p>
+        
+        </div>
+        </div>
     </main>
     <?php $this->view('Layout/Scripts'); ?>
     <script src="/resources/scripts/main.js"></script>
